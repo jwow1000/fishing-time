679158bbb45757e976b7811355923108eaa7d168
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2024-11-27 10:08:33 -0500

    add swiper

---
+++ src/main.js
@@ -0,0 +1,38 @@
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// make sure webflow content is loaded to DOM
+Webflow.push(function() {
+  // select container from webflow
+  const container = document.querySelector(".container");
+
+  // initalize swiper
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  
+});
+
